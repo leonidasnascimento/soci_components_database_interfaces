0f19356f193d62e8879dfc73e41c649afd0561af
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2020-02-24 15:19:55 -0300

    1.2.4

--- src/interfaces/IDataProvider.ts
+++ src/interfaces/IDataProvider.ts
@@ -3,12 +3,14 @@ import { Output } from "../classes/Output";
 import { EnumConnectionState } from "../enum/EnumConnectionState";
 
 export interface IDataProvider {
-    // Methods
-    Select<TOutput>(command: Input): Output<TOutput> | undefined;
-    Add(command: Input): Output<boolean> | undefined;
-    CustomScriptedCommand<TOutput>(command: Input): Output<TOutput> | undefined;
-    CustomFunctionCommand<TDataRepo, TOutput>(_inputFunction: (dataRepoClient?: TDataRepo) => Output<TOutput> | undefined): Output<TOutput> | undefined
-    Connect(): boolean;
-    Disconnect(): boolean;
-    GetConnectionState(): EnumConnectionState;
+  // Methods
+  Select<TOutput>(command: Input): Output<TOutput> | undefined;
+  Add(command: Input): Output<boolean> | undefined;
+  CustomScriptedCommand<TOutput>(command: Input): Output<TOutput> | undefined;
+  CustomFunctionCommand<TDataRepo, TOutput>(
+    _inputFunction: (dataRepoClient?: TDataRepo) => Output<TOutput> | undefined
+  ): Output<TOutput> | undefined;
+  Connect(): boolean;
+  Disconnect(): boolean;
+  GetConnectionState(): EnumConnectionState;
 }
